44360af6e71ddcec3e142a5c22ce5d478a878cc9
Helena Silkina <helenasilkina@gmail.com>
2015-10-07 22:36:54 +0300

    Create Array.fill.js

---
+++ Array.fill.js
@@ -0,0 +1,19 @@
+if (!Array.prototype.fill) {
+  Array.prototype.fill = function(value) {
+    var O = Object(this);
+    var len = O.length >>> 0;
+    var start = arguments[1];
+    var relativeStart = start >> 0;
+    var k = relativeStart < 0 ? Math.max(len + relativeStart, 0) : Math.min(relativeStart, len);
+    var end = arguments[2];
+    var relativeEnd = end === undefined ? len : end >> 0;
+    var final = relativeEnd < 0 ? Math.max(len + relativeEnd, 0) : Math.min(relativeEnd, len);
+
+    while (k < final) {
+      O[k] = value;
+      k++;
+    }
+
+    return O;
+  };
+}
