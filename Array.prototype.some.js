69aff3646e5478b77a2f9e0d245de3347b74caeb
Helena Silkina <helenasilkina@gmail.com>
2015-10-08 13:14:01 +0300

    Update Array.prototype.some.js

--- Array.prototype.some.js
+++ Array.prototype.some.js
@@ -1,12 +1,15 @@
 if (!Array.prototype.some) {
-  Array.prototype.some = function(callback) {
+  Array.prototype.some = function(callback, callbackArguments) {
     'use strict';
     var array = Object(this);
     var length = t.length >>> 0;
-    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    
+    if (arguments.length > 1) {
+      var thisArguments = callbackArguments;
+    }
     
     for (var i = 0; i < length; i++) {
-      if (i in array && callback.call(thisArg, array[i], i, array)) {
+      if (i in array && callback.call(thisArguments, array[i], i, array)) {
         return true;
       }
     }
