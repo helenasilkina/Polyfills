3cb7fe2ac783049d9664c7cddf331b4158c3118b
Helena Silkina <helenasilkina@gmail.com>
2015-10-10 12:43:19 +0300

    Update Array.prototype.forEach.js

--- Array.prototype.forEach.js
+++ Array.prototype.forEach.js
@@ -1,15 +1,12 @@
 if (!Array.prototype.forEach) {
     Array.prototype.forEach = function (function) {
-        var array = Object(this),
-            length = array.length >>> 0;
-            
-        if (typeof function !== 'function') {
-            throw new TypeError();
-        }
+        var array = Object(this);
+        var length = array.length >>> 0;
+        var thisArguments = arguments[1];
         
         for (var i = 0; i < length; i ++) {
             if (array.hasOwnProperty(i)) {
-                function.call(arguments[1], array[i], i, array);
+                function.call(thisArguments, array[i], i, array);
             }
         }
         return undefined;
