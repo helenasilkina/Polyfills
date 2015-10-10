cb7112554de9219fa3e7f821f837ecc92f527868
Helena Silkina <helenasilkina@gmail.com>
2015-10-10 12:47:27 +0300

    Update Array.prototype.some.js

--- Array.prototype.some.js
+++ Array.prototype.some.js
@@ -2,7 +2,7 @@ if (!Array.prototype.some) {
   Array.prototype.some = function(callback, callbackArguments) {
     'use strict';
     var array = Object(this);
-    var length = t.length >>> 0;
+    var length = array.length >>> 0;
     var thisArguments = arguments[1];
     
     for (var i = 0; i < length; i++) {
