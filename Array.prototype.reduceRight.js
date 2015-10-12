e0bfa533df68a58cddfc6cb903206d5fb7d19c63
Helena Silkina <helenasilkina@gmail.com>
2015-10-12 14:34:54 +0300

    Create Array.prototype.reduceRight.js

---
+++ Array.prototype.reduceRight.js
@@ -0,0 +1,30 @@
+if (!Array.prototype.reduceRight) {
+  Array.prototype.reduce = function(callback) {
+    'use strict';
+    
+    var array = Object(this);
+    var length = array.length >>> 0;
+    var index = 0;
+    var value;
+
+    if (arguments.length == 2) {
+      value = arguments[1];
+    } else {
+      while (index < length && !(index in array)) {
+        index--; 
+      }
+      if (index >= length) {
+        throw new TypeError('Reduce of empty array with no initial value');
+      }
+      value = array[index--];
+    }
+    
+    for (; index < length; index--) {
+      if (index in array) {
+        value = callback(value, array[index], index, array);
+      }
+    }
+    
+    return value;
+  };
+}
